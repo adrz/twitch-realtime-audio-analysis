b15998dad1ac6db121350ea910b34bdac0f9f3ab
dude <adrien.nouvellet@gmail.com>
2019-05-16 15:37:16 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Installation
+
+```bash
+virtualenv -p python3 env
+source env/bin/activate
+pip install -r requirements.txt
+```
+
+# Run
+
+```bash
+source env/bin/activate
+python main --url https://www.twitch.tv/zombiunicorn
+```
---
+++ main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+import click
+from src.realtime_audiostreamer import AudioStreamer
+
+@click.command()
+@click.option('--url', '-u', help='url of twitch stream')
+def main(url):
+    AudioStreamer(twitch_url=url)
+
+if __name__ == '__main__':
+    main()
