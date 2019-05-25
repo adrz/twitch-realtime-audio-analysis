c0405a6a4600648f612ecfdb722b8844b5a9b58f
adrien.nouvellet@gmail.com <adrien.nouvellet@gmail.com>
2019-05-25 14:32:18 +0200

    add producers_orchestrator.

--- producer/app.py
+++ producer/app.py
@@ -1,12 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask
-from src.api import api_blueprint
-
-
-app = Flask(__name__)
-app.register_blueprint(api_blueprint)
+from src.audiostreamers_manager import AudioStreamersManager
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    manager = AudioStreamersManager()
--- producers_orchestrator/src/api.py
+++ producers_orchestrator/src/api.py
@@ -2,26 +2,26 @@
 
 from flask import Blueprint
 from flask import jsonify
-from .audiostreamers_manager import AudioStreamersManager
+from .orchestrator import ProducersOrchestrator
 
 
 api_blueprint = Blueprint('streams', __name__)
-manager = AudioStreamersManager()
+orchestrator = ProducersOrchestrator()
 
 
 @api_blueprint.route('/get_streams')
 def list_all():
-    return jsonify(manager.get_streamers())
+    return jsonify(orchestrator.get_streamers())
 
 
 # TODO check error send code 200, or...
 @api_blueprint.route('/add/<name>')
 def add_stream(name: str):
-    manager.add_streamer(name)
+    orchestrator.add(name)
     return jsonify({'added': name})
 
 
 @api_blueprint.route('/del/<name>')
 def rm_stream(name: str):
-    manager.remove_streamer(name)
+    orchestrator.delete(name)
     return jsonify({'removed': name})
